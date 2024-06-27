90c9a2548a943137435cb9e5ff56ba10153eae3f
andreaszamanian <zamanian.andreas@gmail.com>
2024-06-27 12:12:05 -0700

    Edited read_dexcom function
    *works properly for example dataset, still need to add formal test

--- R/read_dexcom.R
+++ R/read_dexcom.R
@@ -18,50 +18,85 @@ read_dexcom <- function(x) {
   random_num <- sample(c(-4,-3,-2,-1,1,2), 1)
   z = shift_time(z, random_num)
 
-  z = reformat(z)
-  #adding columns
-    #record id
-    #parse_id(x)
-    #bg_date_time
-    #bg_value_num
-    #bg_value_glaf
+  z = reformat(z, parse_id(x))
   return(z)
 }
 
-#renames columns per guidelines in our doc
 
-reformat <- function(x){
+parse_id <- function(x) {
+  file_name <- x
+  file_name = as.character(file_name)
+  record_id = substr(file_name, nchar(file_name)-9, nchar(file_name)-4)
+  return(record_id)
+}
+
+#renames/restructures columns per guidelines in doc
+
+reformat <- function(x, y){
   frame <- x
-  #renames time stamp column
-  colnames(frame)[which(names(fram) == "Timestamp (YYYY-MM-DDThh:mm:ss")] <- "bg_date_time"
+  loc_record_id <- y
 
-  #renames blood glucose column, then splits into two columns
-  #one which holds numeric values
-  #other holds "high"/"low" characters if attained
-  colnames(frame)[which(names(fram) == "Glucose Value (mg/dL")] <- "bg_value_num"
-  #new column for boundary values "high" and "low"
-  frame |>
-    mutate(
-      bg_value_flag = bg_value_num
-    )
+  colnames(frame)[which(names(frame) == "Timestamp (YYYY-MM-DDThh:mm:ss)")] <- "bg_date_time" #renames time stamp column
+  frame[, "record_id"] <- loc_record_id #makes new column with every row saying the record id
 
+  #frame1 <- frame
+  #View(frame1)
+
+
+  colnames(frame)[which(names(frame) == "Glucose Value (mg/dL)")] <- "bg_value_num"
+  frame[, "bg_value_flag"] <- NA #new column for boundary values "high" and "low"
+
+  #frame2 <- frame
+  #View(frame2)
+
+  i <- 1
   while(i <= length(frame$Index)){
-
-    if(as.type(frame$'bg_value_num') == "char"){
-
+    if(is.na(frame[i, "bg_value_num"]) != TRUE &&
+       substr(frame[i, "bg_value_num"], 1, 1) != "1" &&
+       substr(frame[i, "bg_value_num"], 1, 1) != "2" &&
+       substr(frame[i, "bg_value_num"], 1, 1) != "3" &&
+       substr(frame[i, "bg_value_num"], 1, 1) != "4" &&
+       substr(frame[i, "bg_value_num"], 1, 1) != "5" &&
+       substr(frame[i, "bg_value_num"], 1, 1) != "6" &&
+       substr(frame[i, "bg_value_num"], 1, 1) != "7" &&
+       substr(frame[i, "bg_value_num"], 1, 1) != "8" &&
+       substr(frame[i, "bg_value_num"], 1, 1) != "9" ) {
+        frame[i, "bg_value_flag"] <- frame[i, "bg_value_num"]
     }
     i = i+1
   }
 
 
+
+  #frame3 <- frame
+  #View(frame3)
+
+  #if I want to just return the four doc columns
+  #COMMENT THIS OUT if I want these columns back in
+  frame <- subset(frame, select = -c(`Index`,
+                                     `Event Type`,
+                                     `Event Subtype`,
+                                     `Device Info`,
+                                     `Source Device ID`,
+                                     `Insulin Value (u)`,
+                                     `Carb Value (grams)`,
+                                     `Duration (hh:mm:ss)`,
+                                     `Glucose Rate of Change (mg/dL/min)`,
+                                     `Transmitter Time (Long Integer)`,
+                                     `Transmitter ID`)
+                  )
+
+  return(frame)
 }
 
 #taking out top three row to remove PHI
-#takes a df as inputloa
+#takes a df as input
 remove_phi <- function(x) {
   frame <- x
   deid_frame <- dplyr::filter(frame, frame$Index >= 4)
   deid_frame$Index <- deid_frame$Index - 3 #shifts index so that it starts at 1 again
+  #remove patient info column (unnecessary now)
+  deid_frame <- subset(deid_frame, select = -`Patient Info`)
   return(deid_frame)
 }
 
@@ -107,12 +142,4 @@ shift_time <- function(x, y){
 #}
 
 #if given file name
-parse_id <- function(x) {
-  file_name <- x
-  file_name = as.character(file_name)
-  record_id = substr(file_name, 1, nchar(file_name)-4)
-}
-
-
-
 
