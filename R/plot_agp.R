e69775d9c62992430a2a5a264cfb86ee2a41ea01
andreaszamanian <zamanian.andreas@gmail.com>
2024-07-19 09:40:47 -0700

    *finished compute_wear_time function *worked on plot_agp function, still not finished *added a dependency *added documentation

--- R/plot_agp.R
+++ R/plot_agp.R
@@ -1,30 +1,110 @@
-plot_agp <- function(dt_agp, months){
-  dt_agp %>%
-    dplyr::filter(inter %in% months) %>%
-    dplyr::mutate(inter = factor(inter, levels = months),
-           bg_level = forcats::fct_rev(bg_level),
-           bg_level = forcats::fct_recode(
-             bg_level,
-             '<54 mg/dL' = '[0,54)',
-             '54-69 mg/dL' = '[54,70)',
-             '70-180 mg/dL' = '[70,181)',
-             '181-250 mg/dL' = '[181,251)',
-             '>250 mg/dL' = '[251,Inf]'),
-           txt = paste0(round(100*bg_level_dist,1), '%')) %>%
-    arrange(inter, desc(bg_level)) %>%
-    dplyr::group_by(inter) %>%
-    dplyr::mutate(cumpct = cumsum(bg_level_dist)) %>%
-    dplyr::ungroup() %>%
-    ggplot() +
-    geom_col(aes(x = inter, y = bg_level_dist, fill = bg_level), color = 'black') +
-    # geom_text(aes(x = inter, y = cumpct, label = txt)) +
-    labs(x = 'Time (months)',
-         y = '% Glucose Range',
-         fill = '') +
-    scale_fill_manual(values = c('#fd8d3c', '#fecc5c', '#a6d96a', '#d7191c', '#a6611a')) +
-    scale_y_continuous(labels = scales::percent)
-}
+#' Canonical bar plot for AGP Thermometers
+#'
+#' @param df read_dexcom output
+#' @param inter time interval
+#'
+#' @return
+#' @export
+#'
+#' @examples
+plot_agp <- function(df, inter){
+  #recompute the different ranges, in format of compute_agp,
+  #but with individual ranges
+  df <- convert_bv(df)
+  df <- set_inter(df, interval = inter)
 
-#where is bg_level_dist defined???
-#check against tir definitions
+  df_1 <- df #for future use in initializing n_value vector
+  df$`bg_value_num` <- as.numeric(df$`bg_value_num`) #write this into read_dexcom function
+
+  dt_agp <- df %>%
+    dplyr::group_by(inter) %>% #redundant
+    dplyr::mutate(range = cut(bg_value_num, c(0, 54, 70, 181, 251, Inf),
+                              include.lowest = T, labels = T)) %>%
+    dplyr::summarise(
+      n = dplyr::n(),
+      range1 = sum(range == [0,54]),
+      range2 = sum(range == [54,70]),
+      range3 = sum(range == [70,181]),
+      range4 = sum(range == [181, 251]),
+      range5 = sum(range == [251, Inf]), #check this interval
+       .groups = 'drop') %>%
+    dplyr::mutate(across(range1:range5, ~ ./n))
+
+  print(dt_agp)
+
+  print("Initial df computed ") ############################
+  #can make all of this cleaner?
+  i <- 1
+  while(i <= max(inter, na.rm = T)){
+    print("I am inside")
+    #map each inter row to a new df
+    j <- as.character(i)
+
+    title <- paste("time period", j, sep = " ")
+    dt_agp_a <- dt_agp %>%
+      dplyr::filter(inter == i) #need to make sure that typeof(inter) is numeric
+
+    print("First part of first while loop completed") ############################
+
+    #store n value
+    n_value <- vector(mode = "integer", length = length(`df_1`$inter))
+    n_value[i] = dt_agp[1, 2]
+
+    #'vectorize' this
+    k <- 1
+    bg_level_dist_a <- vector(mode = "double", length = 5)
+    while(k <= 5){
+      print(dt_agp_a[1, k+2])
+      bg_level_dist_a[k] <- dt_agp_a[1, k+2]
+      k = k+1
+    }
+
+    dt_agp_2 <- tibble::tibble(
+      .rows = 5,
+      range = c(1,2,3,4,5),
+      bg_level_dist = as.numeric(bg_level_dist_a)
+    )
+    #assign the modified dt_agp to the title name; have NOT checked if this works yet
+    assign(title, dt_agp_2)
+
+    if(is.na(dt_agp_2[1, 2]) == T || is.na(dt_agp_2[2, 2]) == T ||
+       is.na(dt_agp_2[3, 2]) == T || is.na(dt_agp_2[4, 2]) == T ||
+       is.na(dt_agp_2[5, 2]) == T){
+      print("Due to missing values plot cannot be produced")
+    }
+    else{
+      #make the plot for the df using method below
+      make_plot(dt_agp_2)
+      #store that plot somewhere?
+    }
+    print("loop iteration finished") ############################
+    i = i+1
+  }
+}
+#bg_level_dist is the proportions for the time in whatever range
+#bg_level is which range in, I changed all calls to bg_level to 'range'
+
+make_plot <- function(df){
+  print("making a plot") ############################
+  df %>%
+    dplyr::mutate(range = as.factor(df$range), #I lost the underlying levels in above code, need to fix this to make below work
+                  range = forcats::fct_recode(
+                    range,
+                    '<54 mg/dL' = '[0,54)',
+                    '54-69 mg/dL' = '[54,70)',
+                    '70-180 mg/dL' = '[70,181)',
+                    '181-250 mg/dL' = '[181,251)',
+                    '>250 mg/dL' = '[251,Inf]'),
+                  txt = paste0(round(100*bg_level_dist,1), '%')) %>%
+    dplyr::mutate(cumpct = cumsum(bg_level_dist)) %>% #not sure I need this
+    dplyr::ungroup() %>%
+    ggplot2::ggplot() +
+    ggplot2::geom_col(aes(x = "testing x", y = bg_level_dist, fill = range), color = 'black') +
+    #geom_text(aes(x = "testing x", y = cumpct, label = txt)) +
+    ggplot2::labs(x = 'Time (months)',
+                  y = '% Glucose Range',
+                  fill = '') +
+    ggplot2::scale_fill_manual(values = c('#fd8d3c', '#fecc5c', '#a6d96a', '#d7191c', '#a6611a')) +
+    ggplot2::scale_y_continuous(labels = scales::percent)
+}
 
